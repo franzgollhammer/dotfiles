4a71cc5c4748a9853fa115d4b036faad75aaf418
Franz Gollhammer <33837460+franzgollhammer@users.noreply.github.com>
2024-01-02 22:10:55 +0100

    eslint formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,12 +2,17 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+    local on_attach = function()
+
+    end
 
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.eslint_d,
 			},
 		})
 
