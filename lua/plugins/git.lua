25b50c7aaec3e219e3a13bc66497da0876d6ebd4
Franz Gollhammer <33837460+franzgollhammer@users.noreply.github.com>
2024-01-03 00:39:06 +0100

    git push pull keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,6 +4,8 @@ return {
     config = function()
       vim.api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
       vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>gps", ":Git push<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>gpl", ":Git pull<CR>", { noremap = true, silent = true })
     end,
   },
   {
