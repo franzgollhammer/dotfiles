0a1b1171fafa4ef00b397f6a75dce6017a5b5a61
Franz Gollhammer <33837460+franzgollhammer@users.noreply.github.com>
2024-01-03 00:37:13 +0100

    git keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,8 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
--- Follow the leader
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Remove default
+-- keymap("", "<Space>", "<Nop>", opts)
 
 -- Modes
 --  normal_mode = "n",
