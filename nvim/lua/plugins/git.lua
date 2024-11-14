2fb1eef6f9031e755383df3c3e5bbb0e19abb530
Franz Gollhammer <33837460+franzgollhammer@users.noreply.github.com>
2024-11-14 17:20:31 +0100

    git keybind

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -16,7 +16,7 @@ return {
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      { "<C-g>", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
   {
